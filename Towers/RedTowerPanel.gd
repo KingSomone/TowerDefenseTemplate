b8e5f00d1791d355a4eb653ac9810bd6e5bd2142
KingSomeone <bheald@ftstudent.org>
2024-09-27 14:06:18 -0400

    update 2

--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -11,13 +11,15 @@ func _on_gui_input(event:InputEvent):
 		add_child(tempTower)
 
 		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+		
+		tempTower.scale = Vector2(0.32,0.32)
+		
 	elif event is InputEventMouseMotion and event.button_mask == 1:
 		#Left Click Drag
 		get_child(1).global_position = event.global_position
 
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		#Left Click Up
-		print("keep yourself safe")
 		get_child(1).queue_free()
 
 		var path = get_tree().get_root().get_node("MainGame/Towers")
